9810b271dfd56e3746ab6740d58f678a585a8529
Jeremy <jrob1time@gmail.com>
2022-12-16 11:15:50 -0600

    update css and html for base layout

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let board, turn, winner, tie
+
+/*------------------------ Cached Element References ------------------------*/
+
+const slotEls = document.querySelectorAll('.sqr')
+const messageEl = document.getElementById('message')
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.querySelector('.board').addEventListener('click', handleClick)
+
+resetBtnEl.addEventListener('click', init)
+
+/*-------------------------------- Functions --------------------------------*/
+init()
+function init() {
+  board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+}
+init()
+function init() {
+  board = [null, null, null, null, null, null, null, null, null, ]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+}
